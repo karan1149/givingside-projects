82c2f71839f2aaa5a50f335ae0016ae14de3bf3a
karan1149 <karan1149@gmail.com>
2017-04-28 12:53:27 -0700

    Working graph visualization template.

---
+++ render.js
@@ -0,0 +1,28 @@
+sigma.parsers.json('data.json', {
+  container: 'container',
+  settings: {
+    defaultNodeColor: '#ec5148'
+  }
+  },
+  function(s) {
+    s.bind('clickNode', function(e) {
+     alert(e.data.node.label);
+      // s.refresh();
+    });
+
+    /* // When the stage is clicked, we just color each
+    // node and edge with its original color.
+    s.bind('clickStage', function(e) {
+      s.graph.nodes().forEach(function(n) {
+        n.color = n.originalColor;
+      });
+
+      s.graph.edges().forEach(function(e) {
+        e.color = e.originalColor;
+      });
+
+      // Same as in the previous event:
+      s.refresh();
+    }); */
+
+});
