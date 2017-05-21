e244d997f4465fadfdded87c26c0a236a238c67c
karan1149 <karan1149@gmail.com>
2017-05-21 10:57:52 -0700

    Added canvas renderer and disabled clicking on "You" node

--- profile/render.js
+++ profile/render.js
@@ -1,28 +1,19 @@
 sigma.parsers.json('data.json', {
-  container: 'container',
+
   settings: {
     defaultNodeColor: '#ec5148'
-  }
+  },
+  renderer: {
+    container: 'container',
+    type: 'canvas'
+  },
   },
   function(s) {
     s.bind('clickNode', function(e) {
-     swal(e.data.node.label);
+      if (e.data.node.label != "You")
+        swal(e.data.node.label);
       // s.refresh();
     });
 
-    /* // When the stage is clicked, we just color each
-    // node and edge with its original color.
-    s.bind('clickStage', function(e) {
-      s.graph.nodes().forEach(function(n) {
-        n.color = n.originalColor;
-      });
-
-      s.graph.edges().forEach(function(e) {
-        e.color = e.originalColor;
-      });
-
-      // Same as in the previous event:
-      s.refresh();
-    }); */
 
 });
